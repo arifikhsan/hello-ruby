f41eb3b6de40835f0694d2c2e927aaaa64591583
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-09-05 11:32:31 +0700

    done

--- candy.rb
+++ candy.rb
@@ -1,28 +1,74 @@
-puts '==============================='
-puts '        EKOJI Challenge        '
-puts '==============================='
+def validate
+  [@price, @exchange_threshold, @exchange_quantity, @budget].map do |object|
+    if object == 0
+      abort "Input tidak valid."
+    elsif object <= 0
+      abort "Angka tidak boleh minus."
+    end
+  end
+end
 
-puts "Algoritma bungkus permen\n\n"
+def how_much_exchange(wrap)
+  exchange_count = wrap / @exchange_threshold
+  wrap_remain = wrap % @exchange_threshold
+  @each_candy_obtained = exchange_count * @exchange_quantity
 
-puts 'Masukkan harga satu permen: ' 
-price = gets.chomp.to_i # X
+  @wrapper_left -= wrap # ditukarkan
+  @wrapper_left += wrap_remain # sisa bungkus
+  @wrapper_left += @each_candy_obtained # dapat permen berapa dari tukaran
+  @candy_obtained += @each_candy_obtained
 
-puts "\n"
+  if @wrapper_left >= @exchange_threshold
+    how_much_exchange(@wrapper_left) # rekursi
+  else
+    return
+  end
+end
 
-puts '-------- Skema Bonus --------'
-puts 'Setiap ... permen: ' 
-exchange_threshold = gets.chomp.to_i # Y
-puts 'Dapat ditukar dengan ... permen: ' 
-exchange_quantity = gets.chomp.to_i # Z
+def resolve
+  @candy_obtained = 0
+  @wrapper_left = 0
+  @each_candy_obtained = 0
 
-puts 'Masukkan jumlah uang untuk dibelanjakan: ' 
-budget = gets.chomp.to_i # W
-puts '-----------------------------'
+  @candy_obtained += @budget / @price
+  @wrapper_left = @candy_obtained.dup
 
-p price.class
-p exchange_threshold.class
-p exchange_quantity.class
-p budget.class
+  initial_candy_wrapper = @wrapper_left
 
-puts 'Total permen yang didapat: ...'
-puts 'Sisa bungkus yang disimpan: ...'
+  how_much_exchange(initial_candy_wrapper)
+end
+
+def show
+  puts '==============================='
+  puts '        EKOJI Challenge        '
+  puts '==============================='
+
+  puts "Algoritma bungkus permen\n\n"
+
+  puts 'Masukkan harga satu permen: '
+  @price = gets.chomp.to_i # X
+
+  puts "\n"
+
+  puts '-------- Skema Bonus --------'
+  puts 'Setiap ... permen: '
+  @exchange_threshold = gets.chomp.to_i # Y
+  puts 'Dapat ditukar dengan ... permen: '
+  @exchange_quantity = gets.chomp.to_i # Z
+  puts '-----------------------------'
+
+  puts "\n"
+
+  puts 'Masukkan jumlah uang untuk dibelanjakan: '
+  @budget = gets.chomp.to_i # W
+
+  puts "\n"
+
+  validate
+  resolve
+
+  puts "Total permen yang didapat: #{@candy_obtained}"
+  puts "Sisa bungkus yang disimpan: #{@wrapper_left}"
+end
+
+show
