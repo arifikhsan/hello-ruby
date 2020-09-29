d0b4858a2483b513be30b5dcd8b522ad6d628ef5
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-09-29 21:12:13 +0700

    comma

--- hello/print.rb
+++ hello/print.rb
@@ -10,3 +10,5 @@ print ['1', ["2", 3]]
 print "\n"
 a = {a: 2}
 print a
+print "\n"
+print 'a', 'b'
--- hello/puts.rb
+++ hello/puts.rb
@@ -6,3 +6,5 @@ puts [1, 2, ['a', 'b']]
 a = {a: 1}
 puts a
 puts [1, 2, ['a', 'b'], {a: 1}]
+puts 'a', 'b'
+puts 'a', 2
